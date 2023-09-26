4dc7cac2393e29cc789bdcbfb33048bd9dcbaea2
Stan <stanislaus.mueller@siemens.com>
2023-09-26 15:34:46 +0200

    do not crate Etag during cration of files

--- webdav/webdav.go
+++ webdav/webdav.go
@@ -291,22 +291,12 @@ func (h *Handler) handlePut(w http.ResponseWriter, r *http.Request) (status int,
 	}
 	_, copyErr := io.Copy(f, r.Body)
 	closeErr := f.Close()
-	fi, statErr := f.Stat()
-	// TODO(rost): Returning 405 Method Not Allowed might not be appropriate.
 	if copyErr != nil {
 		return http.StatusMethodNotAllowed, copyErr
 	}
 	if closeErr != nil {
 		return http.StatusMethodNotAllowed, closeErr
 	}
-	if statErr != nil {
-		return http.StatusMethodNotAllowed, statErr
-	}
-	etag, err := findETag(ctx, h.FileSystem, h.LockSystem, reqPath, fi)
-	if err != nil {
-		return http.StatusInternalServerError, err
-	}
-	w.Header().Set("ETag", etag)
 	return http.StatusCreated, nil
 }
 
